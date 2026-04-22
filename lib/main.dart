e55e628b3bce1cb016af36d0462e2f115f9f4664
hidayat1816 <hidahasanah265@gmail.com>
2026-04-22 13:57:50 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'The Flutter Way - Foodly UI Kit',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: primaryColor),
