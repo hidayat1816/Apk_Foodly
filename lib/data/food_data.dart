831e4806f9d80c625b0b5d3b783f56f71469ffca
julitaliita <julitajulita715@gmail.com>
2026-04-22 19:46:01 -0700

    Create food_data.dart

---
+++ lib/data/food_data.dart
@@ -0,0 +1,20 @@
+List<Map<String, dynamic>> foodList = [
+  {
+    "name": "Burger",
+    "price": 20000,
+    "rating": 4.5,
+    "image": "assets/images/burger.png",
+  },
+  {
+    "name": "Pizza",
+    "price": 50000,
+    "rating": 4.8,
+    "image": "assets/images/pizza.png",
+  },
+  {
+    "name": "Nasi Goreng",
+    "price": 25000,
+    "rating": 4.7,
+    "image": "assets/images/nasigoreng.png",
+  },
+];
