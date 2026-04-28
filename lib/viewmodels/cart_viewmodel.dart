d570720fe3588b1aab1bed4869b41039c4e679a8
julitaliita <julitajulita715@gmail.com>
2026-04-28 03:28:22 -0700

    Update cart_viewmodel.dart

--- lib/viewmodels/cart_viewmodel.dart
+++ lib/viewmodels/cart_viewmodel.dart
@@ -20,4 +20,27 @@ class CartViewModel extends ChangeNotifier {
       notifyListeners();
     }
   }
+
+  /// 🔥 TAMBAH KE CART
+  void addToCart(Map<String, dynamic> item) {
+    cartItems.add(item);
+    notifyListeners();
+  }
+
+  /// 🔥 HAPUS ITEM
+  void removeFromCart(int index) {
+    cartItems.removeAt(index);
+    notifyListeners();
+  }
+
+  /// 🔥 TOTAL HARGA
+  int getTotal() {
+    int total = 0;
+
+    for (var item in cartItems) {
+      total += item["price"] as int;
+    }
+
+    return total;
+  }
 }
