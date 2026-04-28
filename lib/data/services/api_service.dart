cbe48bd1460b06e91f08976d62f62f263bf2d80d
hidayat1816 <hidahasanah265@gmail.com>
2026-04-28 10:34:28 +0700

    Create api_service.dart

---
+++ lib/data/services/api_service.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  final String baseUrl =
+      "https://api.ppb.widiarrohman.my.id/api/2026/uts/B/kelompok4";
+
+  Future<String> getWelcomeText() async {
+    final response = await http.get(Uri.parse("$baseUrl/check"));
+
+    if (response.statusCode == 200) {
+      return response.body; // hasil: "UTS Kelas B - Kelompok 4"
+    } else {
+      throw Exception("Failed to load data");
+    }
+  }
+}
