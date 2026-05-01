92d9b8a2c8eaa73558b501859ee208a783c4930f
julitaliita <julitajulita715@gmail.com>
2026-04-30 23:18:28 -0700

    Update product_viewmodel.dart

--- lib/viewmodels/product_viewmodel.dart
+++ lib/viewmodels/product_viewmodel.dart
@@ -6,6 +6,7 @@ class ProductViewModel extends ChangeNotifier {
   final ApiService _api = ApiService();
 
   List<ProductModel> products = [];
+  List<ProductModel> filteredProducts = []; // 🔥 TAMBAHAN
   bool isLoading = true;
 
   Future<void> fetchProducts() async {
@@ -14,12 +15,30 @@ class ProductViewModel extends ChangeNotifier {
       notifyListeners();
 
       products = await _api.getProducts();
+
+      // 🔥 default: tampilkan semua data
+      filteredProducts = products;
+
     } catch (e) {
       products = [];
+      filteredProducts = [];
       debugPrint("Error Product: $e");
     } finally {
       isLoading = false;
       notifyListeners();
     }
+  }
+
+  // 🔍 🔥 FUNCTION SEARCH (INI YANG PENTING)
+  void searchProduct(String query) {
+    if (query.isEmpty) {
+      filteredProducts = products;
+    } else {
+      filteredProducts = products.where((product) {
+        return product.name.toLowerCase().contains(query.toLowerCase());
+      }).toList();
+    }
+
+    notifyListeners();
   }
 }
